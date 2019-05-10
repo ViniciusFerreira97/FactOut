6d0690330401878f36ee8dba66d19620d4906334
RuanCarlosPorto <ruancarlosporto@hotmail.com>
2019-05-10 18:54:27 -0300

    arrumando grafico

--- public/js/aluno/estatisticas.js
+++ public/js/aluno/estatisticas.js
@@ -1,116 +1,82 @@
 $(document).ready(function () {
 
-  $('#Estatisticas').on('click', function () {
-    $('#estatisticasPosSelecao').hide();
-    $.ajax({
-      url: "/aluno/jfs_finalizado",
-      type: "GET",
-      data: {
-
-      },
-      success: function (result) {
-        $('#estatisticaSlcJF').empty();
-        let aux = "Selecione o JF";
-        $('#estatisticaSlcJF').append('<option value="0">'+ 'Selecione o JF' +'</option>');
-        for (var i = 0; i < result.length; i++) {
-          let option = '<option value="' + result[i]['id_jf'] + '">' + result[i]['id_jf'] + ' - ' + result[i]['nome'] + ' - ' + result[i]['status_jf'] + '</option>';
-          $('#estatisticaSlcJF').append(option);
-        }
-        $('#estatisticaSlcJF').change();
-      }
-    });
-  });
-
-  $('#estatisticaSlcJF').on('change', function () {
-    if($(this).val() == 0)
-      return;
-    $('#estatisticasPosSelecao').show('slide');
-
-    var id_jf = $('#estatisticaSlcJF option:selected').attr("value");
-    $.ajax({
-      url: "/aluno/jfs_corretos_errados",
-      type: "POST",
-      data: {
-        id_jf:id_jf,
-      },
-      success: function (result) {
-        console.log(result);
-        $('#fatosCorretosJF').empty();
-        $('#fatosCorretosJF').append('<option>'+ 'Fatos Corretos' +'</option>');
-        for (var i = 0; i < result['certos'].length; i++) {
-          let option = '<option value="' + result['certos'][i] + '">' + result['certos'][i] + '</option>';
-          $('#fatosCorretosJF').append(option);
-        }
-        $('#fatosCorretosJF').change();
-
-        $('#fatosErradosJF').empty();
-        $('#fatosErradosJF').append('<option>'+ 'Fatos Errados' +'</option>');
-        for (var i = 0; i < result['errados'].length; i++) {
-          let option = '<option value="' + result['errados'][i] + '">' + result['errados'][i] + '</option>';
-          $('#fatosErradosJF').append(option);
-        }
-        $('#fatosErradosJF').change();
-
-        let certos = result['certos'].length;
-        let erros = result['errados'].length;
-
-        var ctxD = document.getElementById("acertos_erros").getContext('2d');
-        var myLineChart = new Chart(ctxD, {
-          type: 'doughnut',
-          data: {
-            labels: ["Erros", "Acertos"],
-            datasets: [{
-              data: [erros, certos],
-              backgroundColor: ["#FF6961", "#77DD77"],
-              hoverBackgroundColor: ["#ff4444", "#00C851"]
-            }]
-          },
-          options: {
-            responsive: true
-          }
+    $('#Estatisticas').on('click', function () {
+        $('#estatisticasPosSelecao').hide();
+        $.ajax({
+            url: "/aluno/jfs_finalizado",
+            type: "GET",
+            data: {},
+            success: function (result) {
+                $('#estatisticaSlcJF').empty();
+                let aux = "Selecione o JF";
+                $('#estatisticaSlcJF').append('<option value="0">' + 'Selecione o JF' + '</option>');
+                for (var i = 0; i < result.length; i++) {
+                    let option = '<option value="' + result[i]['id_jf'] + '">' + result[i]['id_jf'] + ' - ' + result[i]['nome'] + ' - ' + result[i]['status_jf'] + '</option>';
+                    $('#estatisticaSlcJF').append(option);
+                }
+                $('#estatisticaSlcJF').change();
+            }
         });
-
-        let nominal = (certos/(certos+erros));
-        let real = nominal/2;
-        $("#acertosTotais").html(" "+ certos);
-        $("#errosTotais").html(" "+ erros);
-        $("#acertosNominal").html(" "+ (nominal.toFixed(1)) + "%");
-        $("#acertosReal").html(" "+ (real.toFixed(1)) + "%");
-      }
     });
-  });
 
+    $('#estatisticaSlcJF').on('change', function () {
+        if ($(this).val() == 0)
+            return;
+        $('#estatisticasPosSelecao').show('slide');
 
-  var ctxD = document.getElementById("taxaReal").getContext('2d');
-  var myLineChart = new Chart(ctxD, {
-    type: 'doughnut',
-    data: {
-      labels: ["Erros", "Acertos"],
-      datasets: [{
-        data: [7, 3],
-        backgroundColor: ["#FF6961", "#81c784"],
-        hoverBackgroundColor: ["#ff4444", "#77DD77"]
-      }]
-    },
-    options: {
-      responsive: true
-    }
-  });
+        var id_jf = $('#estatisticaSlcJF option:selected').attr("value");
+        $.ajax({
+            url: "/aluno/jfs_corretos_errados",
+            type: "POST",
+            data: {
+                id_jf: id_jf,
+            },
+            success: function (result) {
+                console.log(result);
+                $('#fatosCorretosJF').empty();
+                $('#fatosCorretosJF').append('<option>' + 'Fatos Corretos' + '</option>');
+                for (var i = 0; i < result['certos'].length; i++) {
+                    let option = '<option value="' + result['certos'][i] + '">' + result['certos'][i] + '</option>';
+                    $('#fatosCorretosJF').append(option);
+                }
+                $('#fatosCorretosJF').change();
 
-  var ctxD = document.getElementById("taxaNominal").getContext('2d');
-  var myLineChart = new Chart(ctxD, {
-    type: 'doughnut',
-    data: {
-      labels: ["Erros", "Acertos"],
-      datasets: [{
-        data: [7, 3],
-        backgroundColor: ["#FF6961", "#81c784"],
-        hoverBackgroundColor: ["#ff4444", "#77DD77"]
-      }]
-    },
-    options: {
-      responsive: true
-    }
-  });
+                $('#fatosErradosJF').empty();
+                $('#fatosErradosJF').append('<option>' + 'Fatos Errados' + '</option>');
+                for (var i = 0; i < result['errados'].length; i++) {
+                    let option = '<option value="' + result['errados'][i] + '">' + result['errados'][i] + '</option>';
+                    $('#fatosErradosJF').append(option);
+                }
+                $('#fatosErradosJF').change();
+
+                let certos = result['certos'].length;
+                let erros = result['errados'].length;
+
+                var ctxD = document.getElementById("acertos_erros").getContext('2d');
+                var myLineChart = new Chart(ctxD, {
+                    type: 'doughnut',
+                    data: {
+                        labels: ["Erros", "Acertos"],
+                        datasets: [{
+                            data: [erros, certos],
+                            backgroundColor: ["#FF6961", "#77DD77"],
+                            hoverBackgroundColor: ["#ff4444", "#00C851"]
+                        }]
+                    },
+                    options: {
+                        responsive: true
+                    }
+                });
+
+                let nominal = (certos / (certos + erros));
+                let real = nominal / 2;
+                $("#acertosTotais").html(" " + certos);
+                $("#errosTotais").html(" " + erros);
+                $("#acertosNominal").html(" " + (nominal.toFixed(1)) + "%");
+                $("#acertosReal").html(" " + (real.toFixed(1)) + "%");
+            }
+        });
+    });
+
 
 });
